eda210f7658a925ae1cfb525c43dcbe4cf65cc4d
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2024-04-23 22:16:56 +0200

    modal added for start meeting

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -4,10 +4,12 @@ import { homeLinks } from "@/constants";
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import Card from "./Card";
+import MeetingModal from "./MeetingModal";
 
 const MeetingTypeList = () => {
   const [meetingState, setMeetingState] = useState("");
   const router = useRouter();
+  const createMeeting = () => {};
 
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
@@ -25,6 +27,15 @@ const MeetingTypeList = () => {
           }
         />
       ))}
+
+      <MeetingModal
+        isOpen={meetingState === "isInstantMeeting"}
+        onClose={() => setMeetingState("")}
+        className="text-center"
+        buttonText="Start Meeting"
+        title="Start Instant Meeting"
+        handleClick={createMeeting}
+      />
     </section>
   );
 };
